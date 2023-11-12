678076c7f505934f4c47431c8a9b4afec735e3a1
Dorian Gonzalez <doriandarren1@gmail.com>
2023-11-12 16:16:23 +0100

    componts

--- src/router/index.js
+++ src/router/index.js
@@ -13,6 +13,41 @@ const router = createRouter({
       name: 'alerts',
       component: () => import('@/views/alerts/Alert.vue')
     },
+    {
+      path: '/forms',
+      name: 'forms',
+      component: () => import('@/views/forms/Form.vue')
+    },
+    {
+      path: '/buttons',
+      name: 'buttons',
+      component: () => import('@/views/buttons/Button.vue')
+    },
+    {
+      path: '/badges',
+      name: 'badges',
+      component: () => import('@/views/badges/Badge.vue')
+    },
+    {
+      path: '/cards',
+      name: 'cards',
+      component: () => import('@/views/cards/Card.vue')
+    },
+    {
+      path: '/avatars',
+      name: 'avatars',
+      component: () => import('@/views/avatars/Avatar.vue')
+    },
+    {
+      path: '/paginations',
+      name: 'paginations',
+      component: () => import('@/views/paginations/Pagination.vue')
+    },
+    {
+      path: '/breadcrumbs',
+      name: 'breadcrumbs',
+      component: () => import('@/views/breadcrumbs/Breadcrumb.vue')
+    },
   ]
 })
 
